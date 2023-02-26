a2111c7bfe43d1c156c8167858e59b29b66ca646
Mahmoud Higazy <ma7moudhigazy@gmail.com>
2023-02-26 12:34:48 +0200

    gitignore and some extra fields

--- pizza/models.py
+++ pizza/models.py
@@ -1,5 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Pizzeria(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.
+    CASCADE)
+    address = models.CharField(max_length=512)
+    phone = models.CharField(max_length=40)
 
 class Pizza(models.Model):
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=240)
+    thumbnail_url = models.URLField(null=True, blank=True)
+    approved = models.BooleanField(default=False)
+    creator = models.ForeignKey(Pizzeria, on_delete=models.
+    CASCADE)
+    TYPES = [
+        ('Meat', 'Meat'),
+        ('Vegetarian', 'Vegetarian'),
+        ('Vegan', 'Vegan'),
+    ]
+    types = models.CharField(
+        max_length=12, choices=TYPES, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.
+    CASCADE)
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+import json
 
-# Create your views here.
+def index(request, pid):
+    try:
+        pizza = Pizza.objects.get(id=pid)
+
+        return HttpResponse(
+            content=json.dumps({
+                'id': pizza.id,
+                'title': pizza.title,
+                'description': pizza.description,
+            })
+        )
+    except:
+        return HttpResponse(
+            content=json.dumps({
+                "status": "error",
+                "message": "pizza not found"
+            })
+        )
+
+def random(request):
+    pizzas = Pizza.objects.exclude(likes__user=request.user).order_by("?")[:15].values()
+
+    return HttpResponse(list(pizzas))
